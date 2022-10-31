5576ce5b212145fa7593fcaec92ef11b7fa15d82
GogumaYummy <sora1211x@gmail.com>
2022-11-01 00:20:42 +0900

    내용물 다듬기

--- requestsprac.py
+++ requestsprac.py
@@ -3,4 +3,10 @@ import requests
 r = requests.get('http://spartacodingclub.shop/sparta_api/seoulair')
 rjson = r.json()
 
-print(rjson)
+rows = rjson['RealtimeCityAir']['row']
+
+for row in rows:
+    gu_name = row['MSRSTE_NM']
+    gu_mise = row["IDEX_MVL"]
+    if gu_mise < 60:
+        print(gu_name, gu_mise)
